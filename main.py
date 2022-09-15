5c8ab703f1cfb6edd6b10c123dc973d1a5e1ca9f
jiawei-ren <jiaweiren.gm@gmail.com>
2022-09-15 17:01:47 +0800

    upd

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import streamlit
+import numpy as np
+from brax import envs
+from brax.io import html
+import streamlit.components.v1 as components
+
+env = envs.create(env_name='humanoid')
+state = env.reset(rng=jp.random_prngkey(seed=0))
+components.html(html.render(env.sys, [state.qp]), height=500)
