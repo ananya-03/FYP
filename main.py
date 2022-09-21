00492841ad238479b60b08961e4199f26f0f4fe4
Ananya Singh <67984681+ananya-03@users.noreply.github.com>
2022-09-21 17:28:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from brax.io import html
 import streamlit.components.v1 as components
 import brax.jumpy as jp
 
-env = envs.create(env_name='ant')
+env = envs.create(env_name='humanoid')
 state = env.reset(rng=jp.random_prngkey(seed=0))
 components.html(html.render(env.sys, [state.qp]), height=500)
 
