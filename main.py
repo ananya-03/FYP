00606c86bdb7a506e885f06839366d57e88b42a6
Ananya Singh <67984681+ananya-03@users.noreply.github.com>
2022-09-21 14:15:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ import brax.jumpy as jp
 env = envs.create(env_name='humanoid')
 state = env.reset(rng=jp.random_prngkey(seed=0))
 components.html(html.render(env.sys, [state.qp]), height=500)
+
+%%time
+rollout = []
+for i in range(100):
+  # wiggle sinusoidally with a phase shift per actuator
+  action = jp.sin(i * jp.pi / 15 + jp.arange(0, env.action_size) * jp.pi)
+  state = env.step(state, action)
+  rollout.append(state)
+# jit compile env.step:
+state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
+%%time
+for _ in range(100):
+  state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
+  
+components.html(html.render(env.sys, [s.qp for s in rollout]))
