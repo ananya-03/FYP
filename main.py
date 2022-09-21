e24805714e4fc4877d65702a5eb37654e4f72af9
Ananya Singh <67984681+ananya-03@users.noreply.github.com>
2022-09-21 17:25:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from brax.io import html
 import streamlit.components.v1 as components
 import brax.jumpy as jp
 
-env = envs.create(env_name='halfcheetah')
+env = envs.create(env_name='ant')
 state = env.reset(rng=jp.random_prngkey(seed=0))
 components.html(html.render(env.sys, [state.qp]), height=500)
 
@@ -16,10 +16,10 @@ for i in range(100):
   state = env.step(state, action)
   rollout.append(state)
 # jit compile env.step:
-state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
-# %%time
-for _ in range(100):
-  state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
+# state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
+# # %%time
+# for _ in range(100):
+#   state = jax.jit(env.step)(state, jnp.ones((env.action_size,)))
   
 components.html(html.render(env.sys, [s.qp for s in rollout]),height=500)
 Image(image.render(env.sys, [s.qp for s in rollout], width=320, height=240))
