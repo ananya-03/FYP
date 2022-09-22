9e764e4a2f06ad3e7d0ed4a3a86a70b978035323
Jiawei Ren <72253125+jiawei-ren@users.noreply.github.com>
2022-09-22 14:41:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ env = envs.create(env_name='humanoid')
 state = env.reset(rng=jp.random_prngkey(seed=0))
 # components.html(html.render(env.sys, [state.qp]), height=500)
 
+imporx streamlit
+import brax
+
+streamlit.write(brax.__version__)
 # %%time
 rollout = []
 for i in range(100):
