04f36799cea29e166e95a74610ad02e16846b9d7
Jiawei Ren <72253125+jiawei-ren@users.noreply.github.com>
2022-09-22 14:41:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ env = envs.create(env_name='humanoid')
 state = env.reset(rng=jp.random_prngkey(seed=0))
 # components.html(html.render(env.sys, [state.qp]), height=500)
 
-imporx streamlit
+import streamlit
 import brax
 
 streamlit.write(brax.__version__)
